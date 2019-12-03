ff1b519b2a2b0d2bcbd32dc34d16f85cf0bb5e37
ThanhcongPhung <20160500@student.hust.edu.vn>
2019-12-03 17:40:41 +0700

    add CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,11 +57,12 @@ class Kernel extends HttpKernel
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'cors' => \App\Http\Middleware\Cors::class, // <-- add this line
+
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \App\Http\Middleware\Cors::class // <-- add this line
     ];
 
     /**
